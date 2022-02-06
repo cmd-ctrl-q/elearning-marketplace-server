bc2e79195d6f568ebd89b9776a396ce0618c63dd
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-05 19:11:10 -0600

    create login controller with jwt creation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/user';
 import { hashPassword, comparePassword } from '../utils/auth';
+import jwt from 'jsonwebtoken';
 
 export const Register = async (req, res) => {
   try {
@@ -31,5 +32,43 @@ export const Register = async (req, res) => {
     return res.json({ ok: true });
   } catch (err) {
     return res.status(400).send('Error registering.');
+  }
+};
+
+export const Login = async (req, res) => {
+  try {
+    // get user data
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(400).send('Missing credentials');
+
+    // get user from db
+    const user = await User.findOne({ email }).exec();
+    if (!user) return res.status(400).send('No user found');
+
+    // get user hashed password
+    const { hashedPassword } = user.password;
+
+    // compare passwords
+    const match = await comparePassword(password, hashedPassword);
+    if (!match) {
+      return res.status(400).send('Password is incorrect');
+    }
+
+    // generate JWT with secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    user.password = undefined;
+
+    // add token to cookie
+    res.cookie('token', token, {
+      httpOnly: true,
+      // secure: true, // only https
+    });
+
+    res.json(user);
+  } catch (err) {
+    return res.status(400).send('Error. Try again.');
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-const { Register } = require('../controllers/auth');
+const { Register, Login } = require('../controllers/auth');
 
 router.post('/register', Register);
+router.post('/login', Login);
 
 module.exports = router;
