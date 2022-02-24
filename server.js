7e28c28ec787fa333828bfd81819659e21171441
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-02-24 13:47:28 -0600

    add csrf protection

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import { readdirSync } from 'fs'; // file system
 import mongoose from 'mongoose';
+import csrf from 'csurf';
+import cookieParser from 'cookie-parser';
+
 const morgan = require('morgan');
 require('dotenv').config(); // load env
+
+const csrfProtection = new csrf({ cookie: true });
 
 const app = express();
 
@@ -22,9 +27,14 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(csrfProtection);
 
 // routes
 readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
+app.get('/api/csrf-token', (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
 
 const port = process.env.PORT || 8000;
 
